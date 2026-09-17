141aa473907ee124522cdb4dbad4e22847b956b1
faaliha2007 <faaliha2007@gmail.com>
2026-09-17 21:50:19 +0530

    after adding bank account

--- src/main/java/com/example/banking/BankingApplication.java
+++ src/main/java/com/example/banking/BankingApplication.java
@@ -1,4 +1,5 @@
-package com.example.banking;
+
+		package com.example.banking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +10,5 @@ public class BankingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BankingApplication.class, args);
 	}
-
 }
+
